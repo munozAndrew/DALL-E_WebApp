03b4c0b9d0bef4c5d7939ae9ad8ffdcb8e7f6952
Andrew <arvizumunozandrew@gmail.com>
2024-04-04 19:25:35 -0700

    Generating Image

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const { generateImage } = require("../controllers/openaiController");
 const router = express.Router();
 
+router.post("/generateimage", generateImage);
+/*
 router.post("/generateimage", (req, res) => {
   res.status(200).json({
     success: true,
   });
 });
-
+*/
 module.exports = router;
